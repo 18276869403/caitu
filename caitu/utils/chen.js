2bd57feadf2de9e46263a901a40699060dbd93d9
邓星玉 <1336797684@qq.com>
2020-06-10 09:46:44 +0800

    2020-6-10 哇彩兔

---
+++ caitu/utils/chen.js
@@ -0,0 +1,9 @@
+// const api = require('../utils/config.js')
+const baseUrl = "http://192.168.1.238:8080/jeecg-boot/" 
+ // 我的页面-个人信息查询
+ const my = baseUrl + "rabbit/mine/my"
+
+ module.exports = {
+   baseUrl:baseUrl,
+   my:my
+ } 
---
+++ caitu/utils/config.js
@@ -0,0 +1,12 @@
+//  const baseUrl = "http://192.168.1.238:8080/" 
+ const chen = require('../utils/chen.js')
+//  const baseUrl = "http://192.168.1.238:8080/jeecg-boot/" 
+//  // 我的页面-个人信息查询
+//  const queryPageList = baseUrl + "rabbit/mine/my"
+
+ module.exports = {
+  //  baseUrl:baseUrl,
+  //  queryPageList:queryPageList,
+   chen:chen
+ } 
+
