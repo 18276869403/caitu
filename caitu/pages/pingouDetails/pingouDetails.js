678f8834d85db6788b8cb710660ec90e97a75f6e
薄浩宇 <123456>
2020-06-10 21:38:30 +0800

    1

--- caitu/pages/pingouDetails/pingouDetails.js
+++ caitu/pages/pingouDetails/pingouDetails.js
@@ -11,7 +11,8 @@ Page({
      type: 1, //进行中
     // type:2,//匹配中
     // type:3,//已完成
-
+    pgid:'',
+    pgxxlist:[]
   },
 
   /**
@@ -19,10 +20,40 @@ Page({
    */
   onLoad: function(options) {
     var pgxx=JSON.parse(options.obj)
+    this.data.pgid=pgxx.id
     this.setData({
       pgxx:pgxx
     })
+    this.selectpingouxx()
     console.log(pgxx)
+  },
+  // 获取参与拼购信息
+  selectpingouxx(){
+    var that = this
+    var data = {
+      gbId:that.data.pgid
+    }
+  qingqiu.get('groupBuyingCanYuZhu',data,function(res){
+    console.log(res)
+    if(res.success == true){
+      if (res.result != null) {
+        that.data.pgxxlist=res.result.records
+        for(var i=0;i<res.result.records.length;i++){
+          that.data.pgxxlist[i].createTime=that.data.pgxxlist[i].createtime.split(' ')[0]
+        }
+        console.log(that.data.pgxxlist)
+        that.setData({
+          pgxxlist:that.data.pgxxlist
+        })
+      }else {
+        wx.showToast({
+          title: '暂无数据！',
+          icon:'none',
+          duration:2000
+        })
+      }
+    }
+  })
   },
   // 参与拼购
   joinPingou: function() {
