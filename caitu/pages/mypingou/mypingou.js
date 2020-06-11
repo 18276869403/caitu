4ba6518299bd3cc5b769e168fbcfc431cbe64e2e
admin <1239427696@qq.com>
2020-06-11 09:57:19 +0800

    2

--- caitu/pages/mypingou/mypingou.js
+++ caitu/pages/mypingou/mypingou.js
@@ -9,35 +9,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-    qiugou: [
-      {
-        id: 1,
-        bianhao: 'hz10001',
-        guige: "厚0.5/宽11000000000000",
-        xinceng: "100",
-        didian: "浙江/杭州",
-        shijian: "2020.01.20",
-        type: 1
-      },
-      {
-        id: 2,
-        bianhao: "hz20002",
-        guige: "厚0.5/宽1100",
-        xinceng: "100",
-        didian: "浙江/杭州",
-        shijian: "2020.01.20",
-        type: 2
-      },
-      {
-        id: 3,
-        bianhao: "hz20002",
-        guige: "厚0.5/宽1100",
-        xinceng: "100",
-        didian: "浙江/杭州",
-        shijian: "2020.01.20",
-        type: 3
-      }
-    ],
+    // qiugou: [
+    //   {
+    //     id: 1,
+    //     bianhao: 'hz10001',
+    //     guige: "厚0.5/宽11000000000000",
+    //     xinceng: "100",
+    //     didian: "浙江/杭州",
+    //     shijian: "2020.01.20",
+    //     type: 1
+    //   },
+    //   {
+    //     id: 2,
+    //     bianhao: "hz20002",
+    //     guige: "厚0.5/宽1100",
+    //     xinceng: "100",
+    //     didian: "浙江/杭州",
+    //     shijian: "2020.01.20",
+    //     type: 2
+    //   },
+    //   {
+    //     id: 3,
+    //     bianhao: "hz20002",
+    //     guige: "厚0.5/宽1100",
+    //     xinceng: "100",
+    //     didian: "浙江/杭州",
+    //     shijian: "2020.01.20",
+    //     type: 3
+    //   }
+    // ],
     piugou:[]
   },
 
