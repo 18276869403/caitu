48af5ede94ea5cdd4afd2db31f5a9490cbc0744d
admin <1239427696@qq.com>
2020-06-11 14:18:29 +0800

    1

--- caitu/utils/kaifa.js
+++ caitu/utils/kaifa.js
@@ -10,10 +10,23 @@ const initGroupBuying = baseUrl + "rabbit/index/initGroupBuying"
 const initInformation = baseUrl + "rabbit/index/initInformation"
 // 首页面-尾货
 const initInventory = baseUrl + "rabbit/index/initInventory"
+// 首页面-拼购参与者
+const groupBuyingCanYuZhu = baseUrl + "rabbit/index/groupBuyingCanYuZhu"
+// 大厅-拼购
+const groupByingLists = baseUrl + "rabbit/lobby/groupByingList"
+// 大厅-求购
+const askToBuyLists = baseUrl + "rabbit/lobby/askToBuyList"
+// 大厅-尾货
+const inventoryLists = baseUrl + "rabbit/lobby/inventoryList"
+
  module.exports = {
   groupByingList:groupByingList,
   calculateList:calculateList,
   initGroupBuying:initGroupBuying,
   initInformation:initInformation,
-  initInventory:initInventory
+  initInventory:initInventory,
+  groupBuyingCanYuZhu:groupBuyingCanYuZhu,
+  groupByingLists:groupByingLists,
+  askToBuyLists:askToBuyLists,
+  inventoryLists:inventoryLists
  } 
