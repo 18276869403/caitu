5f20a43a180ee3434314bbe62d7becf530d1227a
Tom <1336797684@qq.com>
2020-06-12 13:47:43 +0800

    1

--- caitu/utils/kaifa.js
+++ caitu/utils/kaifa.js
@@ -1,5 +1,5 @@
 // const api = require('../utils/config.js')
-const baseUrl = "http://111.231.51.198:92/jeecg-boot/" 
+const baseUrl = "http://111.231.51.198:9045/jeecg-boot/" 
  // 我的页面-我的拼购
  const groupByingList = baseUrl + "rabbit/mine/groupByingList"
  // 我的页面-我的计算历史
