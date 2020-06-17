512aec83340baf2e9666e711ac35d8aecdfc558d
邓星玉 <1336797684@qq.com>
2020-06-17 13:08:37 +0800

    1

--- caitu/utils/config.js
+++ caitu/utils/config.js
@@ -2,6 +2,8 @@
  const chen = require('../utils/chen.js')
  const kaifa = require('../utils/kaifa.js')
  const baseUrl = "http://111.231.51.198:9045/jeecg-boot/"  
+//  const baseUrl = "http://192.168.1.244:8080/jeecg-boot/" 
+
  // 图片上传
  const upload = baseUrl + "sys/common/upload/"
  // 图片预览
