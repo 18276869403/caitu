ad853c6163f587759043bd15d48983af2591c4e4
邓星玉 <1336797684@qq.com>
2020-07-06 15:32:24 +0800

    1

--- caitu/utils/util.js
+++ caitu/utils/util.js
@@ -89,6 +89,17 @@ var douleNum = function(obj){
   obj = obj.replace(/^(\-)*(\d+)\.(\d\d).*$/,'$1$2.$3')
   return obj
 }
+const newDate = function(){
+  var timestamp = Date.parse(new Date());
+  var date = new Date(timestamp);
+  //获取年份  
+  var Y =date.getFullYear();
+  //获取月份  
+  var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+  //获取当日日期 
+  var D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate(); 
+  return Y + '-' + M + '-' + D
+}
 
 module.exports = {
   formatTime: formatTime,
@@ -98,5 +109,6 @@ module.exports = {
   arrayStrAdd:arrayStrAdd,
   arrayStrNum:arrayStrNum,
   IdentityNum:IdentityNum,
-  douleNum:douleNum
+  douleNum:douleNum,
+  newDate:newDate
 }
